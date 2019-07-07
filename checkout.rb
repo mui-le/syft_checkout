cc0c713202da69c6afd5754fb632a9bbabc23364
Marcelo Vilela <marcelo@predictchurn.io>
2019-07-07 10:41:24 +0100

    add dummy checkout class

---
+++ checkout.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+#
+# This class will handle the checkout process.
+#
+# As per the requirement, this class will be initialized with the promotional
+# rules. After the objected is instantiated it will be able to receive the scan
+# method to add product items to checkout and the method total will output the
+# value with the discount included
+#
+
+class Checkout
+  def initialize(promotional_rules)
+  end
+end
