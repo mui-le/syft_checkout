85bcd6897cbaa9e95fb47382cf24f42d06414906
Marcelo Vilela <marcelo@predictchurn.io>
2019-07-07 11:06:55 +0100

    add product test

---
+++ specs/product_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+
+describe Product do
+  before do
+    Product.initialize_from_config
+  end
+
+  let(:products) { Product.all }
+
+  describe 'loads from configuration' do
+    it 'has the correct number of records' do
+      expect(products.count).to eq 3
+    end
+  end
+
+  describe 'creates the object' do
+    it 'responds to readers' do
+      product = Product.new({'id':2,'code': 2,
+                              'name': 'item',
+                              'price': 2
+                              })
+
+      expect(product).to have_attributes(id: 2,code: 2,name: 'item',price: 2)
+    end
+
+    describe 'class methods' do
+      it 'responds to all' do
+        products = Product.all
+        expect(products.count).to be > 1
+      end
+
+      it 'responds to find' do
+        product = Product.find(1)
+        expect(product.id).to eq(1)
+      end
+    end
+  end
+end
